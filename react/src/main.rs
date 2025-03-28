279f4c34dae514d570bced3dc67f4fb4de389311
muyaho-universe <kimdaesk@gmail.com>
2025-03-28 15:23:51 +0900

    print out add

--- react/src/main.rs
+++ react/src/main.rs
@@ -84,7 +84,7 @@ fn test_each_cve(
     let prefix = format!("{}/{}/{}", BITCODE_DIR.as_str(), cve.project, cve.id);
     let (bitcode_path1, bitcode_path2) = (prefix.clone() + "_vuln.bc", prefix + "_patch.bc");
     println!("bitcode path: {} {}", bitcode_path1, bitcode_path2);
-    return ;
+    return vec![] ;
     let mut ir_analysis = IRAnalysis2::new(&bitcode_path1, &bitcode_path2, source_diff_path);
     tests
         .iter()
